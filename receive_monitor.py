5ad2633b18140afde68aeea03808d6e97219049e
jinbeom, jeong <ppgt1225@naver.com>
2025-02-01 13:49:24 +0900

    fix recevie monitor

--- receive_monitor.py
+++ receive_monitor.py
@@ -31,15 +31,16 @@ if __name__ == "__main__":
     receive_img_thread.start()
 
     while True:
-        raw_img = decode_jpeg(torch.from_numpy(jpg_bytearr), mode=ImageReadMode.RGB, device='CPU')  # for nvidia JPEG decoding of x86-64 platform
-        # raw_img = nv_jpeg.decode(compressed_img)  # for nvidia JPEG decoding of jetson
-        # raw_img = cv2.imdecode(np.frombuffer(compressed_img, dtype=np.uint8), cv2.IMREAD_COLOR)  # for jpeg-turbo of opencv decoding
-        resized_img = resize_to_fit_display(raw_img, display_width, display_height)
+        if jpg_data_ready:
+            raw_img = decode_jpeg(torch.from_numpy(jpg_bytearr), mode=ImageReadMode.RGB, device='CPU')  # for nvidia JPEG decoding of x86-64 platform
+            # raw_img = nv_jpeg.decode(compressed_img)  # for nvidia JPEG decoding of jetson
+            # raw_img = cv2.imdecode(np.frombuffer(compressed_img, dtype=np.uint8), cv2.IMREAD_COLOR)  # for jpeg-turbo of opencv decoding
+            resized_img = resize_to_fit_display(raw_img, display_width, display_height)
 
-        cv2.imshow(winname='img', mat=resized_img)
+            cv2.imshow(winname='img', mat=resized_img)
 
-        if cv2.waitKey(1) == 'q':  # Exit on pressing 'ESC'
-            break
+            if cv2.waitKey(1) == 'q':  # Exit on pressing 'ESC'
+                break
 
     stop_event.set()
     receive_img_thread.join()
