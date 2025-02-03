c2686c24c8a8ec2d8e0d00628f72e31a6524b23b
jinbeom, jeong <ppgt1225@naver.com>
2025-02-03 13:51:22 +0900

    add save img of receive_monitor.py

--- receive_monitor.py
+++ receive_monitor.py
@@ -1,4 +1,4 @@
-import imagezmq, cv2, threading, torch
+import imagezmq, cv2, threading, torch, itertools
 import numpy as np
 from utils.surround_view import get_screen_resolution, resize_to_fit_display
 from torchvision.io import decode_jpeg, ImageReadMode
@@ -7,6 +7,7 @@ from torchvision.io import decode_jpeg, ImageReadMode
 stop_event = threading.Event()
 jpg_bytearr = None
 jpg_data_ready = False
+img_save_flag = False
 
 
 def receive_image():
@@ -30,14 +31,17 @@ if __name__ == "__main__":
     receive_img_thread = threading.Thread(target=receive_image)
     receive_img_thread.start()
 
-    while True:
+    for i in itertools.count() :
         if jpg_data_ready:
-            raw_img = decode_jpeg(torch.from_numpy(jpg_bytearr), mode=ImageReadMode.RGB, device='CPU')  # for nvidia JPEG decoding of x86-64 platform
+            raw_img = decode_jpeg(torch.from_numpy(jpg_bytearr), mode=ImageReadMode.RGB, device='CPU').numpy()  # for nvidia JPEG decoding of x86-64 platform
             # raw_img = nv_jpeg.decode(compressed_img)  # for nvidia JPEG decoding of jetson
             # raw_img = cv2.imdecode(np.frombuffer(compressed_img, dtype=np.uint8), cv2.IMREAD_COLOR)  # for jpeg-turbo of opencv decoding
             resized_img = resize_to_fit_display(raw_img, display_width, display_height)
 
             cv2.imshow(winname='img', mat=resized_img)
+
+            if img_save_flag:
+                cv2.imwrite(f'save_img\\output_{i}.jpg', raw_img)
 
             if cv2.waitKey(1) == 'q':  # Exit on pressing 'ESC'
                 break
